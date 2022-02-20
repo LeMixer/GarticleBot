68b9ef18d3422a7abe11ce449660153ee6afec9f
LeMixer <78697721+LeMixer@users.noreply.github.com>
2022-02-20 03:49:53 +0100

    refactoring again

--- src/functions.js
+++ src/functions.js
@@ -1,29 +1,27 @@
-const { Embed } = require('@discordjs/builders');
-const { MessageEmbed, Interaction, EmbedFooterData } = require('discord.js');
+// eslint-disable-next-line no-unused-vars
+const { MessageEmbed, Interaction } = require("discord.js");
 
 /**
- * 
- * @param {Interaction} interaction 
- * @returns {MessageEmbed} embed 
+ * @param {Interaction} interaction
+ * @returns {MessageEmbed} embed
  */
 
-function errorEmbed(interaction) {
-    if (!interaction) {
-        throw Error('interaction must be passed as param!');
-    }
+function errorEmbed (interaction) {
+    if (!interaction)
+        throw Error("interaction must be passed as param!");
+
 
     return new MessageEmbed()
-        .setTitle('Exception')
-        .setColor('#e63339')
-        .setDescription('Something went wrong while executing the command.')
+        .setTitle("Exception")
+        .setColor("#e63339")
+        .setDescription("Something went wrong while executing the command.")
         .addField(
-            'Common sources of errrors', '\`bots permissions\`,' + 
-            '\`member not found\`, \`role not found\`, \`incorrect hiercharchy\`', false
+            "Common sources of errrors", "`bots permissions`," +
+            "`member not found`, `role not found`, `incorrect hiercharchy`", false,
         )
-        .setFooter({ text: 'Garticle Bot', iconURL: interaction.guild.me.avatarURL()})
-
+        .setFooter({ text: "Garticle Bot", iconURL: interaction.guild.me.avatarURL() });
 }
 
 module.exports = {
-    errorEmbed
-}
+    errorEmbed,
+};
--- src/index.js
+++ src/index.js
@@ -1,55 +1,59 @@
-const FS = require('fs');
-const { Client, Intents, Collection } = require('discord.js');
-const { token } = require('../config.json');
+const FS = require("fs");
+const { Client, Intents, Collection } = require("discord.js");
+const newLocal = "../config.json";
+const { token } = require(newLocal);
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
 
 
 client.commands = new Collection();
-const COMMAND_FILES = FS.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const COMMAND_FILES = FS.readdirSync("./src/commands").filter(file => file.endsWith(".js"));
 
 for (const FILE of COMMAND_FILES) {
-	const COMMAND = require(`./commands/${FILE}`);
-	client.commands.set(COMMAND.data.name, COMMAND);
+    const COMMAND = require(`./commands/${FILE}`);
+
+    client.commands.set(COMMAND.data.name, COMMAND);
 }
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+    console.log("Ready!");
 });
 
 client
-	.on('warn', console.log)
-	.on('debug', console.log)
+    .on("warn", console.log)
+    .on("debug", console.log);
 
-client.once('warn', (message) =>{
-	console.log(message);
+client.once("warn", message => {
+    console.log(message);
 });
 
-process.on('uncaughtException', (error) =>{
-	console.log(error);
+process.on("uncaughtException", error => {
+    console.log(error);
 });
 
-process.on('unhandledRejection', (error) =>{
-	console.error('Unhandled promise rejection:', error);
+process.on("unhandledRejection", error => {
+    console.error("Unhandled promise rejection:", error);
 });
 
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand())
+        return;
 
-	const command = client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-	
-	if(!command) return;
 
-	try {
-		await command.execute(interaction)
-	} catch (error) {
-		console.error(error);
-		// await interaction.reply({ content: 'There was an error while executing this command!\nPlease contact LearnToChill#4777 for support.', ephemeral: true});
-	}
+    if (!command)
+        return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        // await interaction.reply({ content: 'There was an error while executing this command!\nPlease contact LearnToChill#4777 for support.', ephemeral: true});
+    }
 });
 
 // Login to Discord with your client's token
