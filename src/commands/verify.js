e76a6ac353754fa3c2dc552d0dddab31fc51ea1b
LeMixer <78697721+LeMixer@users.noreply.github.com>
2022-02-20 03:39:09 +0100

    refactoring

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -1,34 +1,32 @@
-const { SlashCommandBuilder} = require('@discordjs/builders');
-const { Interaction, CommandInteraction } = require('discord.js');
-const { errorEmbed } = require('../functions');
-
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { CommandInteraction } = require("discord.js");
+const { errorEmbed } = require("../functions");
 
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('verify')
-        .setDescription('Grants you the community role.'),
+        .setName("verify")
+        .setDescription("Grants you the community role."),
+
     /**
-     * 
-     * @param {CommandInteraction} interaction 
+     *
+     * @param {CommandInteraction} interaction the command interaction
      */
-    async execute(interaction) {
-        
+    async execute (interaction) {
         const role = interaction.guild.roles.cache.find(x => x.name === "Community");
-        const member = interaction.member
+        const { member } = interaction;
 
         if (!role || !member) {
-            await interaction.reply({ embeds: [errorEmbed(interaction)]})
+            await interaction.reply({ embeds: [ errorEmbed(interaction) ] });
+
             return;
         }
 
         if (!member.roles.cache.some(x => x === role)) {
-            
             await member.roles.add(role);
-            await interaction.reply('Verified :ok_hand:');
+            await interaction.reply("Verified :ok_hand:");
+        } else if (member.roles.cache.some(x => x === role)) {
+            await interaction.reply("You are already verified! :x:");
         }
-        else {
-            await interaction.reply('You are already verified! :x:')
-        }
-    }
-}
+    },
+};
